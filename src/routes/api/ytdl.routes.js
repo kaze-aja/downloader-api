90de0213317a639605c09eeb0634ff31fdfcda8c
Kaze aja <michaelagam03@gmail.com>
2024-10-21 10:32:25 +0800

    Update ytdl.routes.js

--- src/routes/api/ytdl.routes.js
+++ src/routes/api/ytdl.routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const router = express.Router();
-
 const YtdlController = require('./../../controllers/api/ytdl.controller');
 
 router.post('/', [check('url').not().isEmpty().withMessage('url is required')], YtdlController.store);
 router.post('/info', [check('url').not().isEmpty().withMessage('url is required')], YtdlController.index);
+
+router.get('/search', [check('query').not().isEmpty().withMessage('Query is required')], YtdlController.search);
 
 module.exports = router;
